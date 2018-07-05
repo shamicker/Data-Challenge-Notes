c69e38b0149baba3a279c5fe57e0379f0de06371
Shauna <shauna.kerr@gmail.com>
2018-07-04 20:38:25 -0400

    Add scripts

---
+++ PythonPart/how_many_snakes.py
@@ -0,0 +1,23 @@
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
+
+name = input('Enter a name: ')
+print("Hello", name.title() + "!")
+
+num = float(input('Enter a number: '))
+num += 20
+print(num)
